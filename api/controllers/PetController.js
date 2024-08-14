3fe3bee57078d92f848d2f96dc3426e24cef141d
adriellyEster <adriellyester9@gmail.com>
2024-08-13 21:59:40 -0300

    criando metodo para adicionar pet

--- api/controllers/PetController.js
+++ api/controllers/PetController.js
@@ -2,14 +2,11 @@ const Pet = require("../models/Pet");
 
 class PetController {
     static async create(req, res) {
-        console.log(req.body)
-
         try {
             const newPet = await Pet.create(req.body);
 
             res.status(201).json(newPet);
         } catch (error) {
-            console.log('=>', error.message);
             res.status(400).json({ error: error.message });
         }
     }
