d4adb30f088296f3a3c3a0639041a3167e31fdba
adriellyEster <adriellyester9@gmail.com>
2024-08-13 22:01:40 -0300

    criando metodo de atualizar dados do pet

--- api/controllers/PetController.js
+++ api/controllers/PetController.js
@@ -21,8 +21,27 @@ class PetController {
         }
     }
 
-    static put(data) {
+    static async put(req, res) {
+        try {
+            const pet = await Pet.findByPk(req.params.id);
 
+            if (pet == null) {
+                res.status(404).json({ erro: "pet não encontrado" });
+                return;
+            }
+
+            const { name, age, description, adoptionStatus } = req.body;
+
+            pet.name = name;
+            pet.age = age;
+            pet.description = description;
+            pet.adoptionStatus = adoptionStatus;
+
+            await pet.save();
+            res.json(pet);
+        } catch (error) {
+            res.status(400).json({ error: error.message });
+        }
     }
 
     static delete(id) {
